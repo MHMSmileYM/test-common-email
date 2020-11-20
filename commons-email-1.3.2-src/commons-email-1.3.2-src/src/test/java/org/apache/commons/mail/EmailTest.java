176f0248ed473a9ef19ad8b360bba0985bf2e220
MHMSmileY <Mostafa.majzoub.mhm@gmail.com>
2020-11-20 12:49:49 -0500

    added AddBcc

---
+++ commons-email-1.3.2-src/commons-email-1.3.2-src/src/test/java/org/apache/commons/mail/EmailTest.java
@@ -0,0 +1,39 @@
+//Mostafa ELMajzoub
+//JUNIT Homework
+package org.apache.commons.mail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmailTest {
+	private static final String[] TEST_EMAILS = { "ab@bc.com", "a.b@c.org", "abcdefghijklmnopqrst@abcdefghijklmnopqrst.com.bd" };
+	private static final String[] TEST_NULL = null;
+	private static final String[] TEST_ENCODING = {"sdadsadsadsa","adssadsadsadsa","sdadsadsa" };
+	private static final String TEST_EMAIL = "ab@bc.com";
+	private static final String TEST_NAME = "test";
+	private static final String TEST_SUBJECT = "test";
+	private static final String TEST_VALUE = "test";	
+	private EmailConcrete email;
+	
+	@Before
+	public void setUpEmailTest() throws Exception {
+		email = new EmailConcrete();
+	}
+	
+	@After
+	public void tearDownEmaiLtest() throws Exception{
+		
+	}
+	
+	@Test
+	public void testAddBcc() throws Exception{
+		email.addBcc(TEST_EMAILS);
+		assertEquals(3, email.getBccAddresses().size());
+		email.addBcc(TEST_NULL);
+		assertEquals(0, email.getBccAddresses().size());
+	}
